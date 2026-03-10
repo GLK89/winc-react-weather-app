bbbfc01119f521f154d8a0fbbdcaea4e0aaef246
Ginger Korporaal <gingerkorporaal@hotmail.com>
2026-03-10 12:26:14 +0100

    Initial commit: Winc React Weather App

---
+++ src/components/WeatherIcon.jsx
@@ -0,0 +1,14 @@
+import "./WeatherIcon.css";
+
+export const WeatherIcon = () => {
+  return (
+    <>
+      <img
+        src="/weather-icons/sunny.png"
+        alt="Sunny weather"
+        className="weather-icon"
+      />
+      <h2>It&apos;s Sunny!</h2>
+    </>
+  );
+};
