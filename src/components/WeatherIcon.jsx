bc3cc64606be9d21f12ceb991c0741e908263abe
Ginger Korporaal <gingerkorporaal@hotmail.com>
2026-03-10 22:00:10 +0100

    Complete weather props exercise

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,14 +1,15 @@
 import "./WeatherIcon.css";
 
-export const WeatherIcon = () => {
+export const WeatherIcon = (props) => {
   return (
     <>
       <img
-        src="/weather-icons/sunny.png"
-        alt="Sunny weather"
+        src={props.weather.imageSrc}
+        alt={props.weather.imageAlt}
         className="weather-icon"
       />
-      <h2>It&apos;s Sunny!</h2>
+
+      <h2>{props.weather.weatherType}</h2>
     </>
   );
 };
