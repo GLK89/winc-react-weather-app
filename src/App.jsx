e25e20aaa25289484b6826f7e80a3db75624a0f5
Ginger Korporaal <gingerkorporaal@hotmail.com>
2026-03-26 13:27:47 +0100

    refactor weather buttons with map and add snowy weather

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,31 @@ import {
   rainyWeather,
   snowyWeather,
 } from "./utils/data";
+
 export const App = () => {
   const [weather, setWeather] = useState(sunnyWeather);
+
+  const weatherObjects = [
+    sunnyWeather,
+    cloudyWeather,
+    rainyWeather,
+    snowyWeather,
+  ];
 
   return (
     <>
       <h1>Winc Weather App</h1>
       <WeatherIcon weather={weather} />
+
       <div className="button-container">
-        <button onClick={() => setWeather(sunnyWeather)}>Sunny</button>
-        <button onClick={() => setWeather(cloudyWeather)}>Cloudy</button>
-        <button onClick={() => setWeather(rainyWeather)}>Rainy</button>
-        <button onClick={() => setWeather(snowyWeather)}>Snowy</button>
+        {weatherObjects.map((weatherObject) => (
+          <button
+            key={weatherObject.weatherType}
+            onClick={() => setWeather(weatherObject)}
+          >
+            {weatherObject.weatherType}
+          </button>
+        ))}
       </div>
     </>
   );
